44bc71464f5288430842a93d965ece7859b4d709
Jorge Galvan <jorgegalvan@Jorges-MacBook-Air.local>
2020-01-03 14:05:28 -0600

    finished assessmet

---
+++ main_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+
+class Item(models.Model):
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('index', kwargs={'list_id': self.id})
---
+++ main_app/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView, DeleteView
+from .models import Item
+
+# Create your views here.
+
+
+def items_index(request):
+    items = Item.objects.all()
+    return render(request, 'index.html', {
+        'items': items
+    })
+
+
+class ItemCreate(CreateView):
+    model = Item
+    fields = '__all__'
+    success_url = '/'
+
+
+class ItemDelete(DeleteView):
+    model = Item
+    success_url = '/'
